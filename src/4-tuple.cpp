d7c14f99664eff49112742ccf8b6b6b0081f5436
Nuffily <147496435+Nuffily@users.noreply.github.com>
2025-05-26 10:02:18 +0500

    Second
    Теперь 4-tuple при работе выводит сообщение каждый раз, проверяя миллиард чисел
    При нажатии CTRL+C программа завершается и выводит последнее проверенное число

--- src/4-tuple.cpp
+++ src/4-tuple.cpp
@@ -1,4 +1,3 @@
-
 #pragma warning(disable : 4146 4244 4828)
 
 #include <iostream>
@@ -7,6 +6,12 @@
 #include <mutex>
 #include <gmpxx.h>
 #include <chrono>
+#include <array>
+#include <set>
+#include <atomic>
+#include <csignal>
+#include <windows.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -15,7 +20,7 @@ mutex cout_mutex;
 auto commence = std::chrono::high_resolution_clock::now();
 
 // Возвращает вектор простых чисел в промежутке от from до limit
-const vector<int> sieve(int limit,int from) {
+const vector<int> sieve(int limit, int from) {
     vector<bool> is_prime(limit + 1, true);
     vector<int> primes;
     for (short p = 2; p * p <= limit; ++p) {
@@ -27,13 +32,14 @@ const vector<int> sieve(int limit,int from) {
     for (int p = from; p <= limit; ++p)
         if (is_prime[p])
             primes.push_back(p);
+
     return primes;
 }
 
 // Проверка на делимость на числа из sieve
-bool is_candidate(const mpz_class& n, const vector<int>& sieve) {
-    for (int p : sieve) {
-        if (mpz_divisible_ui_p(n.get_mpz_t(), p))
+bool is_candidate(const mpz_t n, const vector<int>& sieve) {
+    for (unsigned int p : sieve) {
+        if (mpz_divisible_ui_p(n, p))
             return false;
     }
     return true;
@@ -49,120 +55,238 @@ bool is_candidate(const unsigned int& n, const vector<int>& sieve) {
     return true;
 }
 
+// Strong Prime Probability Test 
+bool SPP2(mpz_t n) {
+    mpz_t t;
+    mpz_t d;
+    mpz_t ff;
+    mpz_t n_minus_1;
+    mpz_t base_2;
+
+    mpz_inits(t, d, ff, n_minus_1, base_2, NULL);
+
+    mpz_sub_ui(n_minus_1, n, 1);
+
+    mpz_set(t, n_minus_1);
+
+    unsigned long s = mpz_scan1(t, 0);
+
+    mpz_fdiv_q_2exp(d, t, s);
+
+    mpz_set_ui(base_2, 2);
+    mpz_powm(ff, base_2, d, n);
+
+    if (mpz_cmp_ui(ff, 1) == 0 || mpz_cmp(ff, n_minus_1) == 0) {
+        mpz_clears(t, d, ff, n_minus_1, base_2, NULL);
+        return true;
+    }
+
+    for (unsigned long i = 1; i < s; ++i) {
+        mpz_powm_ui(ff, ff, 2, n);
+        if (mpz_cmp(ff, n_minus_1) == 0) {
+            mpz_clears(t, d, ff, n_minus_1, base_2, NULL);
+            return true;
+        }
+    }
+
+    mpz_clears(t, d, ff, n_minus_1, base_2, NULL);
+    return false;
+}
+
+/*
+    Поля для остановки программы
+    При нажатии CTRL + C после некоторой задержки выводится последнее пройденное значение, на котором завершилась программа
+*/ 
+bool exit_flag = false;
+mpz_class min_checked("0");
+
+void signal_handler(int signal) {
+    exit_flag = true;
+}
+
+void little_quadruplets(const mpz_class& start) {
+    mpz_class current = start;
+    const int first_quadruplets[12] = { 5, 11, 101, 191, 821, 1481, 1871, 2081, 3251, 3461, 5651, 9431 };
+
+    for (int first_quaruplet : first_quadruplets) {
+        if (current < first_quaruplet) {
+            lock_guard<mutex> lock(cout_mutex);
+
+            std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
+
+            cout << dur.count() << " : Thread " << 1 << " found:\n"
+                << first_quaruplet << "\n" << first_quaruplet + 2 << "\n"
+                << first_quaruplet + 6 << "\n" << first_quaruplet + 8 << "\n\n";
+        
+        }
+    }
+}
+
 /*
  Ищем четверки простых чисел
  В каждом промежутке диаметром 2310 смотрим только числа с определенным шагом,
  остальные не допустимы, так как хоть один элемент четверки делился бы на 2, 3, 5, 7 или 11
- 
+
  cheker - число, равное остатку current (текущего рассматриваемого числа) на prime_mul - произведение некоторых простых чисел (сейчас - от 13 до 36)
- 
+
  cheker - переменная типа int, поэтому проверять ее делимость на малые простые быстрее, чем самого current, а их делимость на заданные простые совпадает
- 
+
  Если все checker { +0, +2, +6, +8} числа прошли делимость на малые простые, начинаем поочередно делить числа предпологаемой четверки
     на простые числа из hard_sieve
- 
+
+ Затем применяем для них СПП2 тест
+
  Если все числа вновь прошли проверку, поочередно проводим для них тест Миллера-Рабина по одному разу
- 
+
  Если и это испытание они прошли, скорее всего, они простые, но для уверенности для каждого запускае еще 24 теста Миллера-Рабина
 
  */
 
 void search_quadruplets(const mpz_class& start, int thread_id, const vector<int>& hard_sieve) {
 
-    const short next[21] = { 90, 30, 210, 90, 90, 210, 30, 90, 90, 120, 120, 90, 90, 30, 210, 90, 90, 210, 30, 90, 11760 };
+    const unsigned short next[21] = { 90, 30, 210, 90, 90, 210, 30, 90, 90, 120, 120, 90, 90, 30, 210, 90, 90, 210, 30, 90, 11760 };
     int iteration = 0;
+    mpz_class temp;
 
-    mpz_class current = start - (start % 2310) + 101 + 2310 * (thread_id - 1);
-    mpz_class current2, current8, current6;
+    if (start < 10000) {
+        if (thread_id == 1) // При вводе слишком маленького числа выводим младшие четверки через little_quadruplets, делать это должен лишь один поток. А поиск начинаем с 10000
+            little_quadruplets(start);
+
+        temp = 10000 - (10000 % 2310) + 101 + 2310 * (thread_id - 7);
+    }
+    else {
+        temp = start - (start % 2310) + 101 + 2310 * (thread_id - 7);
+    }
+
+    mpz_t current, current2, current8, current6;
+
+
+    mpz_init(current);
+    mpz_init(current2);
+    mpz_init(current6);
+    mpz_init(current8);
+
+    mpz_set(current, temp.get_mpz_t());
 
     unsigned int prime_mul = 1;
+    unsigned int billion = 0;
 
     const vector<int> light_sieve = sieve(36, 13);
 
     for (auto& prime : light_sieve)
         prime_mul *= prime;
 
-    unsigned int checker = mpz_class(current % prime_mul).get_si();
+    const unsigned int thershold = 1041872676;
+
+    unsigned int checker = mpz_class(temp % prime_mul).get_si();
+
 
     while (true) {
 
-        if (is_candidate(checker, light_sieve)) {
+        mpz_add_ui(current, current, next[iteration]);
+        checker += next[iteration];
 
-            if (is_candidate(checker + 8, light_sieve)) {
-                
-                if (is_candidate(checker + 6, light_sieve)) {
-                    
-                    if (is_candidate(checker + 2, light_sieve)) {
+        if (iteration > 19) {
 
-                        if (is_candidate(current, hard_sieve)) {
-                            current8 = current + 8;
-                            if (is_candidate(current8, hard_sieve)) {
-                                current6 = current + 6;
-                                if (is_candidate(current6, hard_sieve)) {
-                                    current2 = current + 2;
-                                    if (is_candidate(current2, hard_sieve)) {
+            iteration = 0;
 
-                                        if (mpz_probab_prime_p(current.get_mpz_t(), 1)) {
+            if (checker > thershold) {
+                checker -= thershold;
 
-                                            if (mpz_probab_prime_p(current8.get_mpz_t(), 1)) {
-
-                                                if (mpz_probab_prime_p(current6.get_mpz_t(), 1)) {
-
-                                                    if (mpz_probab_prime_p(current2.get_mpz_t(), 1)) {
-
-                                                        if (mpz_probab_prime_p(current6.get_mpz_t(), 24) &&
-                                                            mpz_probab_prime_p(current8.get_mpz_t(), 24) &&
-                                                            mpz_probab_prime_p(current2.get_mpz_t(), 24) &&
-                                                            mpz_probab_prime_p(current.get_mpz_t(), 24)) {
-
-                                                            if (current > start) {
-                                                                lock_guard<mutex> lock(cout_mutex);
-                                                                
-                                                                cout << "Thread " << thread_id << " found:\n"
-                                                                << current << "\n" << current + 2 << "\n"
-                                                                << current + 6 << "\n" << current + 8 << "\n\n";
-                                                                
-                                                                std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
-                                                                
-                                                                cout << dur.count() << "\n\n";
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                if (thread_id == 1) {
+                    billion++;            // При каждом пройденном миллиарде чисел выводим сообщение об этом в консоль
+                    std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
+                    cout << dur.count() << " : " << billion << " Billion is passed\n" << endl;
                 }
             }
         }
-
-        current += next[iteration];
-        checker += next[iteration];
-        
-        if (iteration > 19)
-            iteration = 0;
-        else
+        else {
             iteration++;
+            if (exit_flag) {
+                lock_guard<mutex> lock(cout_mutex);
 
-        if (checker > prime_mul)
-            checker -= prime_mul;
+                if (min_checked == 0 || min_checked > mpz_class(current))
+                    min_checked = mpz_class(current);
+
+                return;
+            }
+        }
+
+        // Делим на малые простые
+        if (!is_candidate(checker, light_sieve)) continue;
+
+        if (!is_candidate(checker + 8, light_sieve))  continue;
+
+        if (!is_candidate(checker + 6, light_sieve))  continue;
+
+        if (!is_candidate(checker + 2, light_sieve))  continue;
+
+
+        // Делим на не очень малые простые
+        if (!is_candidate(current, hard_sieve)) continue;
+
+        mpz_add_ui(current8, current, 8);
+        if (!is_candidate(current8, hard_sieve)) continue;
+
+        mpz_add_ui(current6, current, 6);
+        if (!is_candidate(current6, hard_sieve)) continue;
+
+        mpz_add_ui(current2, current, 2);
+        if (!is_candidate(current2, hard_sieve)) continue;
+
+        // Тест СПП2
+        if (!(SPP2(current) && SPP2(current8) && SPP2(current6) && SPP2(current2))) continue;
+
+        // Тест Миллера-Рабина
+        if (!mpz_probab_prime_p(current, 1)) continue;
+
+        if (!mpz_probab_prime_p(current8, 1)) continue;
+
+        if (!mpz_probab_prime_p(current6, 1)) continue;
+
+        if (!mpz_probab_prime_p(current2, 1)) continue;
+
+        if (mpz_probab_prime_p(current6, 24) &&
+            mpz_probab_prime_p(current8, 24) &&
+            mpz_probab_prime_p(current2, 24) &&
+            mpz_probab_prime_p(current, 24)) {
+
+            if (mpz_cmp(start.get_mpz_t(), current)) {
+                lock_guard<mutex> lock(cout_mutex);
+
+                std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
+
+                cout << dur.count() << " : Thread " << thread_id << " found:\n"
+                    << current << "\n" << current2 << "\n"
+                    << current6 << "\n" << current8 << "\n\n";
+            }
+        }
+
     }
 }
 
+
+
 int main() {
-    
-    mpz_class base = 13;
-    unsigned long exp = 500;
+
+    signal(SIGINT, signal_handler);                         // Для перехвата CTRL + C
+
+    mpz_class base = 10;
+    unsigned long exp = 1000;
 
     mpz_class z;
     mpz_pow_ui(z.get_mpz_t(), base.get_mpz_t(), exp);       // Текущее число = base ^ exp
 
-    const vector<int> hard_sieve = sieve(1000, 36);
+    // z += mpz_class("140358297211");                        // Добавка (удобно для продолжения с последней точки)
+
+    int digits = mpz_sizeinbase(z.get_mpz_t(), 10);
     
+    if (digits < 10)
+        digits = 10;
+
+
+    const vector<int> hard_sieve = sieve(500 + digits * 20, 37);  // Вектор с простыми числами для проверки
+
     thread t1([&]() { search_quadruplets(z, 1, hard_sieve); });
     thread t2([&]() { search_quadruplets(z, 2, hard_sieve); });
     thread t3([&]() { search_quadruplets(z, 3, hard_sieve); });
@@ -176,6 +300,8 @@ int main() {
     t4.join();
     t5.join();
     t6.join();
+
+    cout << "Program has stopped on number\n" << min_checked << endl;            // Информация о последнем пройденном числе
 
     return 0;
 }
--- src/9-tuple.cpp
+++ src/9-tuple.cpp
@@ -6,6 +6,11 @@
 #include <mutex>
 #include <gmpxx.h>
 #include <chrono>
+#include <array>
+#include <atomic>
+#include <csignal>
+#include <windows.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -14,23 +19,23 @@ mutex cout_mutex;
 auto commence = chrono::high_resolution_clock::now();
 
 // Возвращает вектор простых чисел в промежутке от start до limit
-const vector<short> sieve(int limit, int start) {
+const vector<int> sieve(int limit, int start) {
     vector<bool> is_prime(limit + 1, true);
-    vector<short> primes;
+    vector<int> primes;
     for (int p = 2; p * p <= limit; ++p) {
         if (is_prime[p]) {
             for (int i = p * p; i <= limit; i += p)
                 is_prime[i] = false;
         }
     }
-    for (short p = start; p <= limit; ++p)
+    for (int p = start; p <= limit; ++p)
         if (is_prime[p])
             primes.push_back(p);
     return primes;
 }
 
 // Проверяет, делится ли n на одно из чисел в векторе
-bool is_candidate(const mpz_class& n, const vector<short>& sie) {
+bool is_candidate(const mpz_class& n, const vector<int>& sie) {
     for (short p : sie) {
         if (mpz_divisible_ui_p(n.get_mpz_t(), p))
             return false;
@@ -39,7 +44,7 @@ bool is_candidate(const mpz_class& n, const vector<short>& sie) {
 }
 
 // Проверяет, делится ли n на одно из чисел в векторе
-bool is_candidate(const unsigned long& n, const vector<short>& sie) {
+bool is_candidate(const unsigned long& n, const vector<int>& sie) {
     for (short p : sie) {
         if (!(n % p)) {
             return false;
@@ -49,9 +54,22 @@ bool is_candidate(const unsigned long& n, const vector<short>& sie) {
 }
 
 /*
+    Поля для остановки программы
+    При нажатии CTRL + C после некоторой задержки выводится последнее пройденное значение, на котором завершилась программа
+*/ 
+bool exit_flag = false;
+mpz_class min_checked("0");
+
+void signal_handler(int signal) {
+    exit_flag = true;
+}
+
+/*
  Ищет девятки простых чисел, начиная со start, выводит всю девятку и время, прошедшее с запуска программы
     hard_sieve - вектор простостых чисел, с помощью которых будет проверяться простота чисел
-    
+
+ Алгоритм такой же, как и у поиска четверок
+
 Поиск проходит только по особым числам с особым шагом
     step - диаметр одного цикла поиска
     offs - промежутки между числами, которые нужно проверять (с которых может начинаться девятка)
@@ -59,23 +77,23 @@ bool is_candidate(const unsigned long& n, const vector<short>& sie) {
     start_off - длина от начала цикла до первого нужного числа
     const_offset - показыват шаблон девятки, то есть какая длина между числами в девятке
  */
-void search_quadruplets(const mpz_class& start, int thread_id, const vector<short>& hard_sieve,
-    const short start_off, const short step, const vector<short>& offs, const int offs_last, const std::array<int,8>& const_offset) {
-    
+void search_quadruplets(const mpz_class& start, int thread_id, const vector<int>& hard_sieve,
+    const int start_off, const int step, const vector<int>& offs, const int offs_last, const vector<int>& const_offset) {
+
     mpz_class current[9];
     current[0] = start + (step - start % step) + start_off - step;
 
+    int billions = 0;
     unsigned int prime_mul = 1;
 
-    const vector<short> light_sieve = sieve(40, 14);
+    const vector<int> light_sieve = sieve(40, 14);
 
     for (auto& prime : light_sieve)
         prime_mul *= prime;
 
     unsigned long checker = (prime_mul < current[0]) ? mpz_class(current[0] % prime_mul).get_si() : current[0].get_si();
 
-    short iteration = 0;
-
+    int iteration = 0;
 
     while (true) {
 
@@ -113,79 +131,116 @@ void search_quadruplets(const mpz_class& start, int thread_id, const vector<shor
             }
 
             {
-            lock_guard<mutex> lock(cout_mutex);
-                
-            if (current[0] < start)
-                goto exit_l;
-                
-            cout << "Thread " << thread_id << " found:\n";
-            for (int i = 0; i < 9; i++)
-                cout << current[i] << "\n";
+                lock_guard<mutex> lock(cout_mutex);
 
-            std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
-            cout << "\n" << dur.count() << "\n\n";
+                if (current[0] < start)
+                    goto exit_l;
+
+                cout << "Thread " << thread_id << " found:\n";
+                for (int i = 0; i < 9; i++)
+                    cout << current[i] << "\n";
+
+                std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
+
             }
-            
+
         }
-        exit_l:
+
+    exit_l:
 
         current[0] += offs[iteration];
         checker += offs[iteration];
 
         if (iteration < offs_last)
             iteration++;
-        else
+        else {
             iteration = 0;
 
-        if (checker > prime_mul)
-            checker -= prime_mul;
+            if (checker > 988443308) {
+                checker -= 988443308;
+
+                if (thread_id == 1) {
+                    billions++;
+
+                    if (billions % 10 == 0) {
+                        std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
+                        cout << dur.count() << " : " << billions << " billion has passed by" << "\n";
+
+                    }
+                }
+            }
+
+            if (exit_flag) {
+                lock_guard<mutex> lock(cout_mutex);
+                
+                if (min_checked == 0 || min_checked > current[0])
+                    min_checked = current[0];
+                
+                return;
+            }
+        }
+
     }
 }
 
 int main() {
     mpz_class base = 10;
-    unsigned long exp = 15;
+    unsigned long exp = 100;
+
+    signal(SIGINT, signal_handler);                         // Для перехвата CTRL + C
 
     mpz_class z;
     mpz_pow_ui(z.get_mpz_t(), base.get_mpz_t(), exp);
 
+    const vector<int> super_sie = sieve(400, 40);
 
-    const vector<short> super_sie = sieve(400, 40);
 
-    /* 
-    Шаблона для девяток простых чисел есть всего 4
-    Для каждого шаблона найдены допустимые остатки при делении первого числа на 30030
-    Поиск по каждому шаблону запускает в уникальном потоке
-    */
-    const vector<short> offs_1 = { 2730, 1890, 2730, 840, 3360, 1260, 1050, 420, 2310, 1890, 1260, 1470, 840, 4620, 3360};
-    const std::array<int,8> const_offfs_1 = { 2, 6, 8, 12, 18, 20, 26, 30 };
-    const short start_1 = 1271;
-    const short step_1 = 30030;
+    const vector<int> offs_1 = { 2730, 1890, 2730, 840, 3360, 1260, 1050, 420, 2310, 1890, 1260, 1470, 840, 4620, 3360 };
+    const vector<int> const_offs_1 = { 2, 6, 8, 12, 18, 20, 26, 30 };
+    const int start_1 = 1271;
+    const int step_1 = 30030;
 
-    const vector<short> offs_2 = { 990, 420, 1320, 630, 360, 900, 990, 60, 1680, 990, 630, 1320, 1260, 2040, 270, 420, 2310, 570, 1320, 1410, 630, 690, 570, 60, 990, 1680, 1950, 990, 1260, 1320 };
-    const std::array<int,8> const_offfs_2 = { 4, 6, 10, 16, 18, 24, 28, 30 };
-    const short start_2 = 1273;
-    const short step_2 = 30030;
+    //const vector<int> offs_2 = { 990, 420, 1320, 630, 360, 900, 990, 60, 1680, 990, 630, 1320, 1260, 2040, 270, 420, 2310, 570, 1320, 1410, 630, 690, 570, 60, 990, 1680, 1950, 990, 1260, 1320 };
+    const vector<int> offs_2 = { 990, 420, 1320, 630, 360, 900, 990, 60, 1680, 990, 630, 1320, 1260, 2040, 16170};
+    const vector<int> const_offs_2 = { 4, 6, 10, 16, 18, 24, 28, 30 };
+    const int start_2 = 1273;
+    const int step_2 = 30030;
 
-    const vector<short> offs_3 = { 1260, 990, 1950, 1680, 990, 60, 570, 690, 630, 1410, 1320, 570, 2310, 420, 270, 2040, 1260, 1320, 630, 990, 1680, 60, 990, 900, 360, 630, 1320, 420, 990, 1320 };
-    const std::array<int,8> const_offfs_3 = { 2, 6, 12, 14, 20, 24, 26, 30 };
-    const short start_3 = 17;
-    const short step_3 = 30030;
+    const vector<int> offs_3 = { 1260, 990, 1950, 1680, 990, 60, 570, 690, 630, 1410, 1320, 570, 2310, 420, 270 + 14910 };
+    const vector<int> const_offs_3 = { 2, 6, 12, 14, 20, 24, 26, 30 };
+    const int start_3 = 17;
+    const int step_3 = 30030;
 
-    const vector<short> offs_4 = { 4620, 840, 1470, 1260, 1890, 2310, 420, 1050, 1260, 3360, 840, 2730, 1890, 2730, 3360 };
-    const std::array<int,8> const_offfs_4 = { 4, 10, 12, 18, 22, 24, 28, 30 };
-    const short start_4 = 2059;
-    const short step_4 = 30030;
+    const vector<int> offs_4 = { 4620, 840, 1470, 1260, 1890, 2310, 420, 1050, 1260, 3360, 840, 2730, 1890, 2730, 3360 };
+    const vector<int> const_offs_4 = { 4, 10, 12, 18, 22, 24, 28, 30 };
+    const int start_4 = 2059;
+    const int step_4 = 30030;
 
-    thread t1([&]() { search_quadruplets(z, 1, super_sie, start_1, step_1, offs_1, 14, const_offfs_1); });
-    thread t2([&]() { search_quadruplets(z, 2, super_sie, start_2, step_2, offs_2, 29, const_offfs_2); });
-    thread t3([&]() { search_quadruplets(z, 3, super_sie, start_3, step_3, offs_3, 29, const_offfs_3); });
-    thread t4([&]() { search_quadruplets(z, 4, super_sie, start_4, step_4, offs_4, 14, const_offfs_4); });
-    
+    const vector<int> offs_5 = { 420, 2310, 570, 1320, 1410, 630, 690, 570, 60, 990, 1680, 1950, 990, 1260, 1320 + 13860 };
+    const vector<int> const_offs_5 = { 4, 6, 10, 16, 18, 24, 28, 30 };
+    const int start_5 = 1273 + 13860;
+    const int step_5 = 30030;
+
+    const vector<int> offs_6 = { 2040, 1260, 1320, 630, 990, 1680, 60, 990, 900, 360, 630, 1320, 420, 990, 1320 + 15120 };
+    const vector<int> const_offs_6 = { 2, 6, 12, 14, 20, 24, 26, 30 };
+    const int start_6 = 17 + 15120;
+    const int step_6 = 30030;
+
+    thread t1([&]() { search_quadruplets(z, 1, super_sie, start_1, step_1, offs_1, 14, const_offs_1); });
+    thread t2([&]() { search_quadruplets(z, 2, super_sie, start_2, step_2, offs_2, 14, const_offs_2); });
+    thread t3([&]() { search_quadruplets(z, 3, super_sie, start_3, step_3, offs_3, 14, const_offs_3); });
+    thread t4([&]() { search_quadruplets(z, 4, super_sie, start_4, step_4, offs_4, 14, const_offs_4); });
+    thread t5([&]() { search_quadruplets(z, 5, super_sie, start_5, step_5, offs_5, 14, const_offs_5); });
+    thread t6([&]() { search_quadruplets(z, 6, super_sie, start_6, step_6, offs_6, 14, const_offs_6); });
+
     t1.join();
     t2.join();
     t3.join();
     t4.join();
+    t5.join();
+    t6.join();
+
+    cout << "Program has stopped on number\n" << min_checked << endl;
 
     return 0;
 }
