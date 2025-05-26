72b529b296131b44faae5869f046df7ecce02ea8
Nuffily <147496435+Nuffily@users.noreply.github.com>
2025-05-26 20:35:12 +0500

    Update 9-tuple.cpp

--- src/9-tuple.cpp
+++ src/9-tuple.cpp
@@ -6,62 +6,27 @@
 #include <mutex>
 #include <gmpxx.h>
 #include <chrono>
-#include <array>
 #include <atomic>
 #include <csignal>
 #include <windows.h>
 #include <cstdlib>
+#include "prime_modules.h"
 
 using namespace std;
 
-mutex cout_mutex;
+mutex cout_mutex_9;
 
-auto commence = chrono::high_resolution_clock::now();
-
-// Возвращает вектор простых чисел в промежутке от start до limit
-const vector<int> sieve(int limit, int start) {
-    vector<bool> is_prime(limit + 1, true);
-    vector<int> primes;
-    for (int p = 2; p * p <= limit; ++p) {
-        if (is_prime[p]) {
-            for (int i = p * p; i <= limit; i += p)
-                is_prime[i] = false;
-        }
-    }
-    for (int p = start; p <= limit; ++p)
-        if (is_prime[p])
-            primes.push_back(p);
-    return primes;
-}
-
-// Проверяет, делится ли n на одно из чисел в векторе
-bool is_candidate(const mpz_class& n, const vector<int>& sie) {
-    for (short p : sie) {
-        if (mpz_divisible_ui_p(n.get_mpz_t(), p))
-            return false;
-    }
-    return true;
-}
-
-// Проверяет, делится ли n на одно из чисел в векторе
-bool is_candidate(const unsigned long& n, const vector<int>& sie) {
-    for (short p : sie) {
-        if (!(n % p)) {
-            return false;
-        }
-    }
-    return true;
-}
+chrono::high_resolution_clock::time_point commence_9;
 
 /*
     Поля для остановки программы
     При нажатии CTRL + C после некоторой задержки выводится последнее пройденное значение, на котором завершилась программа
-*/ 
-bool exit_flag = false;
-mpz_class min_checked("0");
+*/
+bool exit_flag_9 = false;
+mpz_class min_checked_9("0");
 
-void signal_handler(int signal) {
-    exit_flag = true;
+void signal_handler_9(int signal) {
+    exit_flag_9 = true;
 }
 
 /*
@@ -77,7 +42,7 @@ void signal_handler(int signal) {
     start_off - длина от начала цикла до первого нужного числа
     const_offset - показыват шаблон девятки, то есть какая длина между числами в девятке
  */
-void search_quadruplets(const mpz_class& start, int thread_id, const vector<int>& hard_sieve,
+void search_tuples_of_nine(const mpz_class& start, int thread_id, const vector<int>& hard_sieve,
     const int start_off, const int step, const vector<int>& offs, const int offs_last, const vector<int>& const_offset) {
 
     mpz_class current[9];
@@ -131,7 +96,7 @@ void search_quadruplets(const mpz_class& start, int thread_id, const vector<int>
             }
 
             {
-                lock_guard<mutex> lock(cout_mutex);
+                lock_guard<mutex> lock(cout_mutex_9);
 
                 if (current[0] < start)
                     goto exit_l;
@@ -140,7 +105,7 @@ void search_quadruplets(const mpz_class& start, int thread_id, const vector<int>
                 for (int i = 0; i < 9; i++)
                     cout << current[i] << "\n";
 
-                std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
+                std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence_9;
 
             }
 
@@ -163,19 +128,19 @@ void search_quadruplets(const mpz_class& start, int thread_id, const vector<int>
                     billions++;
 
                     if (billions % 10 == 0) {
-                        std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence;
+                        std::chrono::duration<double> dur = std::chrono::high_resolution_clock::now() - commence_9;
                         cout << dur.count() << " : " << billions << " billion has passed by" << "\n";
 
                     }
                 }
             }
 
-            if (exit_flag) {
-                lock_guard<mutex> lock(cout_mutex);
-                
-                if (min_checked == 0 || min_checked > current[0])
-                    min_checked = current[0];
-                
+            if (exit_flag_9) {
+                lock_guard<mutex> lock(cout_mutex_9);
+
+                if (min_checked_9 == 0 || min_checked_9 > current[0])
+                    min_checked_9 = current[0];
+
                 return;
             }
         }
@@ -183,17 +148,13 @@ void search_quadruplets(const mpz_class& start, int thread_id, const vector<int>
     }
 }
 
-int main() {
-    mpz_class base = 10;
-    unsigned long exp = 100;
+int seekForNineTuples(mpz_class z) {
 
-    signal(SIGINT, signal_handler);                         // Для перехвата CTRL + C
+    signal(SIGINT, signal_handler_9);                         // Для перехвата CTRL + C
 
-    mpz_class z;
-    mpz_pow_ui(z.get_mpz_t(), base.get_mpz_t(), exp);
+    commence_9 = chrono::high_resolution_clock::now();
 
     const vector<int> super_sie = sieve(400, 40);
-
 
     const vector<int> offs_1 = { 2730, 1890, 2730, 840, 3360, 1260, 1050, 420, 2310, 1890, 1260, 1470, 840, 4620, 3360 };
     const vector<int> const_offs_1 = { 2, 6, 8, 12, 18, 20, 26, 30 };
@@ -201,7 +162,7 @@ int main() {
     const int step_1 = 30030;
 
     //const vector<int> offs_2 = { 990, 420, 1320, 630, 360, 900, 990, 60, 1680, 990, 630, 1320, 1260, 2040, 270, 420, 2310, 570, 1320, 1410, 630, 690, 570, 60, 990, 1680, 1950, 990, 1260, 1320 };
-    const vector<int> offs_2 = { 990, 420, 1320, 630, 360, 900, 990, 60, 1680, 990, 630, 1320, 1260, 2040, 16170};
+    const vector<int> offs_2 = { 990, 420, 1320, 630, 360, 900, 990, 60, 1680, 990, 630, 1320, 1260, 2040, 16170 };
     const vector<int> const_offs_2 = { 4, 6, 10, 16, 18, 24, 28, 30 };
     const int start_2 = 1273;
     const int step_2 = 30030;
@@ -226,12 +187,12 @@ int main() {
     const int start_6 = 17 + 15120;
     const int step_6 = 30030;
 
-    thread t1([&]() { search_quadruplets(z, 1, super_sie, start_1, step_1, offs_1, 14, const_offs_1); });
-    thread t2([&]() { search_quadruplets(z, 2, super_sie, start_2, step_2, offs_2, 14, const_offs_2); });
-    thread t3([&]() { search_quadruplets(z, 3, super_sie, start_3, step_3, offs_3, 14, const_offs_3); });
-    thread t4([&]() { search_quadruplets(z, 4, super_sie, start_4, step_4, offs_4, 14, const_offs_4); });
-    thread t5([&]() { search_quadruplets(z, 5, super_sie, start_5, step_5, offs_5, 14, const_offs_5); });
-    thread t6([&]() { search_quadruplets(z, 6, super_sie, start_6, step_6, offs_6, 14, const_offs_6); });
+    thread t1([&]() { search_tuples_of_nine(z, 1, super_sie, start_1, step_1, offs_1, 14, const_offs_1); });
+    thread t2([&]() { search_tuples_of_nine(z, 2, super_sie, start_2, step_2, offs_2, 14, const_offs_2); });
+    thread t3([&]() { search_tuples_of_nine(z, 3, super_sie, start_3, step_3, offs_3, 14, const_offs_3); });
+    thread t4([&]() { search_tuples_of_nine(z, 4, super_sie, start_4, step_4, offs_4, 14, const_offs_4); });
+    thread t5([&]() { search_tuples_of_nine(z, 5, super_sie, start_5, step_5, offs_5, 14, const_offs_5); });
+    thread t6([&]() { search_tuples_of_nine(z, 6, super_sie, start_6, step_6, offs_6, 14, const_offs_6); });
 
     t1.join();
     t2.join();
@@ -240,7 +201,7 @@ int main() {
     t5.join();
     t6.join();
 
-    cout << "Program has stopped on number\n" << min_checked << endl;
+    cout << "Program has stopped on number\n" << min_checked_9 << endl;
 
     return 0;
 }
